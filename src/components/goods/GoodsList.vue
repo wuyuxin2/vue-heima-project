a5c767d48261385c3877e31e51c7ff19b7146984
wuyuxin2 <564791604@qq.com>
2019-07-24 16:30:58 +0800

    经典的商品两列布局 css3

---
+++ src/components/goods/GoodsList.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="goods-list">
+        <div class="goods-item">
+            <img src="https://farm6.staticflickr.com/5591/15008867125_68a8ed88cc_b.jpg" alt="">
+            <h1 class="title">华为p30 望远镜</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">¥6999</span>
+                    <span class="old">¥9999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+
+            </div>
+        </div>
+        <div class="goods-item">
+            <img src="https://farm6.staticflickr.com/5591/15008867125_68a8ed88cc_b.jpg" alt="">
+            <h1 class="title">华为p30 望远镜华为p30 望远镜华为p30 望远镜华为p30 望远镜</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">¥6999</span>
+                    <span class="old">¥9999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+
+            </div>
+        </div>
+        <div class="goods-item">
+            <img src="https://farm6.staticflickr.com/5591/15008867125_68a8ed88cc_b.jpg" alt="">
+            <h1 class="title">华为p30 望远镜华为p30 望远镜华为p30 望远镜华为p30 望远镜</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">¥6999</span>
+                    <span class="old">¥9999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.goods-list{
+    display: flex;
+    flex-wrap: wrap;
+    // justify-content: space-around; 居中
+    padding: 7px;
+    justify-content: space-between; // 两边贴紧
+    .goods-item{
+        width: 49%;
+        border: 1px solid #ccc;
+        box-shadow: 0 0 8px #ccc;
+        margin: 4px 0;
+        padding: 2px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        min-height: 293px;
+        img{
+            width:100%;
+        }
+        .title{
+            font-size: 14px;
+        }
+        .info{
+            background-color: #ddd;
+            
+            p{
+                margin: 0;
+                padding: 5px;
+            }
+            .price{
+                .now{
+                    color:red;
+                    font-weight: bold;
+                    font-size: 16px;
+                }
+                .old{
+                    text-decoration: line-through;
+                    font-size: 12px;
+                    margin-left: 10px;
+                }
+            }
+            .sell{
+                display: flex;
+                justify-content: space-between;
+                font-size: 12px;
+            }
+        }
+    }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import NewsList from './components/news/NewsList.vue'
 import NewsInfo from './components/news/NewsInfo.vue';
 import PhotoList from './components/photos/PhotoList.vue'
 import PhotoInfo from './components/photos/PhotoInfo.vue'
+import GoodsList from './components/goods/GoodsList.vue'
 // 创建路由对象
 var router = new VueRouter({
     routes: [
@@ -20,6 +21,7 @@ var router = new VueRouter({
         { path: '/home/newsinfo/:id', component: NewsInfo },
         { path: '/home/photolist', component: PhotoList },
         { path: '/home/photoinfo/:id', component: PhotoInfo },
+        { path: '/home/goodslist', component:GoodsList }
     ],
     linkActiveClass: 'mui-active' // 覆盖默认的路由高亮类router-link-active
 })
