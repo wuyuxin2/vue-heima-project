06b9042aeed2b958913100b9257b7b7ab7a7fb2e
wuyuxin2 <564791604@qq.com>
2019-07-25 11:26:19 +0800

    请求数据

--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -4,65 +4,86 @@
     <div class="mui-card">
       <div class="mui-card-content">
         <div class="mui-card-content-inner">
-            <swiper :lunbotuList="lunbotu" :isfull="false"></swiper>
+          <swiper :lunbotuList="lunbotu" :isfull="false"></swiper>
         </div>
       </div>
     </div>
 
     <!-- 商品购买区域 -->
     <div class="mui-card">
-      <div class="mui-card-header">商品名称</div>
+      <div class="mui-card-header">{{ goodsinfo.title }}</div>
       <div class="mui-card-content">
         <div class="mui-card-content-inner">
-            <p class="price">
-                市场价：<del>¥2399</del>&nbsp;&nbsp;销售价<span class="now_price">¥2199</span>
-            </p>
-            <p>购买数量：<numbox></numbox></p>
-            <p>
-                <mt-button type="primary" size="small">立即购买</mt-button>
-                <mt-button type="danger" size="small">假如购物车</mt-button>
-            </p>
+          <p class="price">
+            市场价：
+            <del>¥{{ goodsinfo.market_price }}</del>&nbsp;&nbsp;销售价
+            <span class="now_price">¥{{ goodsinfo.sell_price }}</span>
+          </p>
+          <p>
+            购买数量：
+            <numbox></numbox>
+          </p>
+          <p>
+            <mt-button type="primary" size="small">立即购买</mt-button>
+            <mt-button type="danger" size="small">假如购物车</mt-button>
+          </p>
         </div>
       </div>
     </div>
 
     <!-- 商品参数区域 -->
     <div class="mui-card">
-      <div class="mui-card-header">页眉</div>
+      <div class="mui-card-header">商品参数</div>
       <div class="mui-card-content">
-        <div class="mui-card-content-inner">包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）</div>
+        <div class="mui-card-content-inner">
+          <p>商品货号：{{ goodsinfo.goods_no }}</p>
+          <p>库存情况：{{ goodsinfo.stock_quantity }}件</p>
+          <p>上架时间：{{ goodsinfo.add_time | dateFormat }}</p>
+        </div>
       </div>
-      <div class="mui-card-footer">页脚</div>
+      <div class="mui-card-footer">
+          <mt-button type="primary" size="large" plain>图文介绍</mt-button>
+          <mt-button type="danger" size="large" plain>商品评论</mt-button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import swiper from '../subcomponnets/swiper.vue'
-import numbox from '../subcomponnets/goodsinfo_numbox.vue'
+import swiper from "../subcomponnets/swiper.vue";
+import numbox from "../subcomponnets/goodsinfo_numbox.vue";
 
 export default {
   data() {
     return {
       id: this.$route.params.id,
-      lunbotu: []
+      lunbotu: [],
+      goodsinfo: {}
     };
   },
-  components:{
-      swiper,
-      numbox
+  components: {
+    swiper,
+    numbox
   },
   created() {
     this.getLunbotu();
+    this.getGoodsInfo();
   },
   methods: {
     getLunbotu() {
       this.$http.get("api/getthumimages/" + this.id).then(result => {
         if (result.body.status === 0) {
           result.body.message.forEach(element => {
-              element.img = element.src
+            element.img = element.src;
           });
           this.lunbotu = result.body.message;
+        }
+      });
+    },
+    getGoodsInfo(){
+        this.$http.get("api/goods/getinfo/" + this.id).then(result => {
+        if (result.body.status === 0) {
+          this.goodsinfo = result.body.message[0];
         }
       });
     }
@@ -74,10 +95,17 @@ export default {
 .goodsinfo-container {
   background-color: #eee;
   overflow: hidden;
-  .now_price{
-      color: red;
-      font-size: 16px;
-      font-weight: bold;
+  .now_price {
+    color: red;
+    font-size: 16px;
+    font-weight: bold;
+  }
+  .mui-card-footer{
+      display: block;
+      button{
+          margin: 10px 0;
+      }
+      
   }
 }
 </style>
