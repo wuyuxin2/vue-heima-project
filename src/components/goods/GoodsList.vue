986272fd8a19779d68e48fba0b7a2ce583a1408c
wuyuxin2 <564791604@qq.com>
2019-07-25 08:29:21 +0800

    函数式编程路由

--- src/components/goods/GoodsList.vue
+++ src/components/goods/GoodsList.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="goods-list">
-    <div class="goods-item" v-for="item in goodslist" :key="item.id">
+    <!-- <router-link 
+    class="goods-item" 
+    v-for="item in goodslist" :key="item.id"
+    :to="'/home/goodsinfo/' + item.id"
+    tag="div">
       <img :src="item.img_url"/>
+      <h1 class="title">{{ item.title }}</h1>
+      <div class="info">
+        <p class="price">
+          <span class="now">¥{{ item.sell_price }}</span>
+          <span class="old">¥{{ item.market_price }}</span>
+        </p>
+        <p class="sell">
+          <span>热卖中</span>
+          <span>剩{{ item.stock_quantity }}件</span>
+        </p>
+      </div>
+    </router-link>-->
+
+    <!-- 在网页中有两种跳转方式：
+      1. 使用 a 标签 的形式叫做 标签跳转
+    2. 使用 window.loaction.href 的形式，叫做 编程式导航-->
+    <div @click="goDetail(item.id)" class="goods-item" v-for="item in goodslist" :key="item.id">
+      <img :src="item.img_url" />
       <h1 class="title">{{ item.title }}</h1>
       <div class="info">
         <p class="price">
@@ -40,9 +62,20 @@ export default {
           }
         });
     },
-    getMore(){
-        this.pageindex++;
-        this.getGoodsList();
+    getMore() {
+      this.pageindex++;
+      this.getGoodsList();
+    },
+    goDetail(id) {
+      // 使用 JS 的形式进行路由导航
+
+      // 注意： 区分 this.$route 和 this.$router 这两个对象
+      // 其中： this.$route 是路由【参数对象】，所有路由中的参数，params，query都属于它
+      // 其中： this.$router 是一个路由【导航对象】，用它可以方便的 使用JS代码，实现路由的前进、后退、跳转到新的URL地址
+      console.log(this);
+      // this.$router.push( "/home/goodsinfo/" + id )
+      // this.$router.push({ path: "/home/goodsinfo/" + id })
+      this.$router.push({ name: "goodsinfo", parms: { id } });
     }
   }
 };
